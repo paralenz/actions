1e19c82ed0adcc2f49996ac1feafa82f9b0576c6
Carsten Daurehøj <arelstone@gmail.com>
2021-09-18 12:14:49 +0200

    👷  Auto generate documentation and create new release automatically (#1)

---
+++ scripts/generate-docs.ts
@@ -0,0 +1,64 @@
+import fs from 'fs'
+import path from 'path'
+import glob from 'glob'
+import yaml from 'js-yaml'
+import tableify from 'markdown-tableify'
+
+const files = glob.sync('**/*.yml', { ignore: ['**/node_modules/**', '**/example/*'] })
+
+const read = (path: string) => fs.readFileSync(path, { encoding: 'utf-8' })
+
+files.forEach(file => {
+  const { inputs, name, description } = JSON.parse(JSON.stringify(yaml.load(read(file))))
+
+  const values = Object.keys(inputs)
+    .map(name => ({
+      name,
+      required: inputs[name].required ? 'Yes' : '',
+      defaultValue: inputs[name].default || '',
+      description: inputs[name].description || ''
+    }))
+
+  const table = tableify(values, {
+    headers: [{
+      name: 'name',
+      align: ':---',
+      title: 'Name'
+    },
+    {
+      name: 'required',
+      align: ':---',
+      title: 'Required'
+    },
+    {
+      name: 'defaultValue',
+      align: ':---',
+      title: 'Default'
+    }, {
+      name: 'description',
+      align: ':---',
+      title: 'Description'
+    }]
+  })
+
+  const [folder] = file.split('/')
+  const packageName = `${folder}/README.md`
+  const outPath = path.join(__dirname, '..', packageName)
+
+  const content = `# paralenz/${folder}
+
+${name}
+
+${description}
+    
+${table}
+    `
+
+  try {
+    fs.writeFileSync(outPath, content, { encoding: 'utf-8' })
+
+    console.error(`Generated file: ${packageName}`)
+  } catch (err) {
+    console.error(`Could not write to ${outPath}`)
+  }
+})
