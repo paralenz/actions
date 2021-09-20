ea14a92537122239d261154977f66739ed9ad579
Carsten Daurehøj <arelstone@gmail.com>
2021-09-20 08:05:17 +0200

    commit

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -16,8 +16,8 @@ files.forEach(file => {
   const values = inputs && Object.keys(inputs)
     .map(name => ({
       name,
-      required: inputs[name].required ? 'Yes' : '',
-      defaultValue: inputs[name].default || '',
+      required: inputs[name].required ? '**Yes**' : 'No',
+      defaultValue: `\`${inputs[name].default}\`` || '',
       description: inputs[name].description || ''
     }))
 
@@ -54,17 +54,26 @@ ${description}
     
 ### Usage:
 \`\`\`yaml
-  - uses: paralenz/${folder}@${version}
+  - uses: paralenz/${folder}@master
+  
+  # or lock to a specific version
+  - uses: paralenz/${folder}@v${version}
 \`\`\`
 
+${table ? 'Options' : ''}
 ${table ?? ''}
+
+<hr />
+
+This documentation is generated automaticlly. All edits of this file will be overwritten on the next release.
+Please do noo edit this file.
     `
 
   try {
     fs.writeFileSync(outPath, content, { encoding: 'utf-8' })
 
-    console.error(`Generated file: ${packageName}`)
+    console.error(`🎉 Generated file: ${packageName}`)
   } catch (err) {
-    console.error(`Could not write to ${outPath}`)
+    console.error(`🔥 Could not write to ${outPath}`)
   }
 })
