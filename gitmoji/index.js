ff5e2e6864f6ecae1bbdeaeecf02aa0546b53e1f
Carsten Daurehøj <arelstone@gmail.com>
2021-11-16 11:52:24 +0100

    ⚗️ Fetch gitmojis form origin (#20)
    * Fetch gitmojies form origin
    
    * Set working dir
    
    * Add checkout and setup-node
    
    * commit
    
    * wrap name in colon
    
    * no need to log
    
    * commit
    
    * parse as string
    
    * Bumnp version
    
    * Add inputs

---
+++ gitmoji/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const core = require('@actions/core')
+const axios = require('axios').default
+
+axios.get('https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
+  .then(r => r.data)
+  .then(({ gitmojis }) => Object.keys(gitmojis).map(key => {
+    const { name, emoji } = gitmojis[key]
+
+    return [
+      emoji,
+      `:${name}:`
+    ].join(',')
+  }))
+  .then(r => r.join(','))
+  .then(gitmojis => core.setOutput('gitmojis', `${gitmojis}`))
